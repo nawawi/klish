acf6aaf536f4dc2f017c3b6513820a05ea6014a2
Serj Kalichev <serj.kalichev@gmail.com>
2010-10-20 09:49:09 +0000

    Add 'test' utility implementation from OpenBSD. The test.c is public domain.
    git-svn-id: https://klish.googlecode.com/svn/trunk@247 0eaa4687-2ee9-07dd-09d9-bcdd2d2dd5fb

---
+++ lub/system.h
@@ -0,0 +1,20 @@
+/*
+ * system.h
+ *
+ */
+#ifndef _lub_system_h
+#define _lub_system_h
+
+#include <stddef.h>
+
+#include "lub/c_decl.h"
+_BEGIN_C_DECL
+
+bool_t lub_system_test(int argc, char **argv);
+bool_t lub_system_argv_test(const lub_argv_t *argv);
+bool_t lub_system_line_test(const char *line);
+
+_END_C_DECL
+
+#endif /* _lub_system_h */
+
---
+++ lub/system/private.h
@@ -0,0 +1,9 @@
+/*
+ * private.h
+ */
+#include "lub/types.h"
+#include "lub/argv.h"
+#include "lub/system.h"
+
+int testcmd(int argc, char *argv[]);
+
