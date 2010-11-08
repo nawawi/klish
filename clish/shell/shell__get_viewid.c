0ad3258672bbf5ac02c866348e2429c224a41d38
Serj Kalichev <serj.kalichev@gmail.com>
2010-11-08 10:59:39 +0000

    Assertions
    git-svn-id: https://klish.googlecode.com/svn/trunk@286 0eaa4687-2ee9-07dd-09d9-bcdd2d2dd5fb

--- clish/shell/shell__get_viewid.c
+++ clish/shell/shell__get_viewid.c
@@ -1,11 +1,15 @@
 /*
  * shell__get_viewid.c
  */
+
+#include <assert.h>
+
 #include "private.h"
 
 /*--------------------------------------------------------- */
 const char *clish_shell__get_viewid(const clish_shell_t * this)
 {
+	assert(this);
 	return this->viewid;
 }
 
