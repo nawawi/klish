cb12375950a9fde54675ebb80d18d4e00cb3a8c9
Serj Kalichev <serj.kalichev@gmail.com>
2011-07-06 11:36:18 +0000

    Print warning when the watchdog is active
    git-svn-id: https://klish.googlecode.com/svn/trunk@498 0eaa4687-2ee9-07dd-09d9-bcdd2d2dd5fb

--- clish/shell/shell_execute.c
+++ clish/shell/shell_execute.c
@@ -307,6 +307,7 @@ int clish_shell_execute(clish_context_t *context, char **out)
 	if (this->wdog_timeout && saved_wdog_timeout) {
 		tinyrl__set_timeout(this->tinyrl, this->wdog_timeout);
 		this->wdog_active = BOOL_TRUE;
+		fprintf(stderr, "The watchdog is active. Press any key to stop it.\n");
 	} else
 		tinyrl__set_timeout(this->tinyrl, this->idle_timeout);
 
