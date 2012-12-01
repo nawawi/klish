1681d4d200611db64bffac5fe814435320d4c5d2
Serj Kalichev <serj.kalichev@gmail.com>
2012-12-02 01:44:33 +0400

    Unfinished plugin.h

---
+++ clish/plugin/private.h
@@ -0,0 +1,20 @@
+/*
+ * plugin private.h
+ */
+
+#include "clish/plugin.h"
+
+/*---------------------------------------------------------
+ * PRIVATE TYPES
+ *--------------------------------------------------------- */
+
+struct clish_sym_s {
+	char *name; /* Symbol name */
+	clish_plugin_fn_t *func; /* Function address */
+};
+
+struct clish_plugin_s {
+	char *file; /* Plugin file name. Must be unique. */
+	char *name; /* Local plugin name. Can be used in builtin ref. */
+	lub_list_t *syms; /* List of plugin symbols */
+};
