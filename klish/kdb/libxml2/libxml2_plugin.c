8bbfdb0f535cc4d0b4dd622827ad7d3a21e39622
Serj Kalichev <serj.kalichev@gmail.com>
2021-03-19 21:49:24 +0300

    xml: Statically build xml helper

--- klish/kdb/libxml2/libxml2_plugin.c
+++ klish/kdb/libxml2/libxml2_plugin.c
@@ -6,3 +6,17 @@
 #include <faux/faux.h>
 #include <faux/str.h>
 #include <klish/kxml.h>
+#include <klish/kscheme.h>
+
+
+kscheme_t *kxml_load_scheme(const char *xml_path, faux_error_t *error);
+
+
+bool_t kdb_libxml2_init(void)
+{
+	kscheme_t *scheme = NULL;
+
+	scheme = kxml_load_scheme(NULL, NULL);
+scheme = scheme;
+	return BOOL_TRUE;
+}
