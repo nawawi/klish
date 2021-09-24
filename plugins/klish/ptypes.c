ded33b681e48bfebf5e42bdc2e3cb852c5d2344f
Serj Kalichev <serj.kalichev@gmail.com>
2021-09-24 13:37:58 +0300

    Add lost files

---
+++ plugins/klish/ptypes.c
@@ -0,0 +1,36 @@
+/*
+ *
+ */
+
+#include <assert.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#include <faux/str.h>
+#include <faux/list.h>
+#include <klish/kcontext.h>
+#include <klish/kentry.h>
+
+
+int klish_ptype_COMMAND(kcontext_t *context)
+{
+	kparg_t *parg = NULL;
+	const kentry_t *entry = NULL;
+	const char *value = NULL;
+	const char *command_name = NULL;
+
+	parg = kpargv_candidate_parg(kcontext_parent_pargv(context));
+	entry = kparg_entry(parg);
+	value = kparg_value(parg);
+
+	command_name = kentry_value(entry);
+	if (!command_name)
+		command_name = kentry_name(entry);
+	if (!command_name)
+		return -1;
+
+	return faux_str_casecmp(value, command_name);
+}
