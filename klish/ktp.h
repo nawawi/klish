e45f29d52ee374e61e162b7bd256030a31ce86c0
Serj Kalichev <serj.kalichev@gmail.com>
2022-09-02 18:03:11 +0300

    Add KTP_PARAM_PROMPT

--- klish/ktp.h
+++ klish/ktp.h
@@ -34,6 +34,7 @@ typedef enum {
 	KTP_PARAM_NULL = '\0',
 	KTP_PARAM_LINE = 'L',
 	KTP_PARAM_PREFIX = 'P', // Same as line but differ by meaning
+	KTP_PARAM_PROMPT = '$', // Same as line but differ by meaning
 	KTP_PARAM_ERROR = 'E',
 	KTP_PARAM_RETCODE = 'R',
 } ktp_param_e;
